633b23674f23f4a05e0983193c360d36e190ae97
Josh Bartman <josh.bartman@gmail.com>
2022-08-14 12:10:11 -0700

    Reject empty strings

--- server/validation/validation.js
+++ server/validation/validation.js
@@ -1,3 +1,24 @@
-const { check } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 
-exports.validate_last_name = [check("last_name").trim().escape()];
+exports.validate_last_name = [
+  check("last_name")
+    .trim()
+    .escape()
+    .not()
+    .isEmpty()
+    .withMessage("User name can not be empty!"),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({
+        error: true,
+        errors: errors.array(),
+        message: errors
+          .array()
+          .map((a) => a.msg)
+          .join(" "),
+      });
+    next();
+  },
+];
