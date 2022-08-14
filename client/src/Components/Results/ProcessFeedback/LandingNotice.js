5e1387cb19146f7b3d330742c5bd847c62df4695
Josh Bartman <josh.bartman@gmail.com>
2022-08-14 15:24:33 -0700

    Add welcome alter for initial page load

---
+++ client/src/Components/Results/ProcessFeedback/LandingNotice.js
@@ -0,0 +1,10 @@
+import { Alert } from "react-bootstrap";
+
+const LandingNotice = () => {
+  return (
+    <Alert variant="success" className="flex-center">
+      Welcome, enter a last name above to begin your search
+    </Alert>
+  );
+};
+export default LandingNotice;
--- client/src/Components/Results/Results.js
+++ client/src/Components/Results/Results.js
@@ -1,5 +1,6 @@
 import EmployeeList from "./EmployeeList";
 import Error from "./ProcessFeedback/Error";
+import LandingNotice from "./ProcessFeedback/LandingNotice";
 import Loading from "./ProcessFeedback/Loading";
 import NoResult from "./ProcessFeedback/NoResults";
 import Success from "./ProcessFeedback/Success";
@@ -12,6 +13,8 @@ const Results = (props) => {
     message = <Loading />;
   } else if (error) {
     message = <Error error={error} />;
+  } else if (undefined === searchTerm) {
+    return <LandingNotice />;
   } else if (0 === data.length) {
     message = <NoResult searchTerm={searchTerm} />;
   } else if (!(0 === data.length)) {
