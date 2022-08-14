3c3e38d8324b549030f6a656270a07c924c7300e
Josh Bartman <josh.bartman@gmail.com>
2022-08-14 12:12:00 -0700

    Fix ssl and display errors via initialOptions

--- server/util/database.js
+++ server/util/database.js
@@ -1,18 +1,26 @@
-const pgp = require("pg-promise")();
+const initOptions = {
+  error(err, e) {
+    console.log("initOptions", err, e);
+  },
+};
+
+const pgp = require("pg-promise")(initOptions);
 const { ConnectionString } = require("connection-string");
 
 const connectionObj = new ConnectionString(
-  process.env.DATABASE_URL ||
-    "hostname:postgres://localhost:5432/uaw4121;id=root;Password='';"
+  process.env.DATABASE_URL || "postgres://@localhost:5432/uaw4121"
 );
-
+let ssl = null;
+if (process.env.NODE_ENV === "production") {
+  ssl = { rejectUnauthorized: false };
+}
 const cn = {
   host: connectionObj.hostname,
   port: connectionObj.port,
   database: connectionObj.path?.[0],
   user: connectionObj.user,
   password: connectionObj.password,
-  ssl: false,
+  ssl: ssl,
 };
 
 exports.db = pgp(cn);
